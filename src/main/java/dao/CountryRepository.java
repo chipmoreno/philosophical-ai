c86095354cf3b5ec0fc12d666be183804b190ec3
chipmoreno <chipmoreno@Chips-MacBook-Air.local>
2024-11-22 15:14:55 -0600

    E.   Write code for the dao package that includes repository interfaces for the entities that extend JpaRepository, and add cross-origin support.

---
+++ src/main/java/dao/CountryRepository.java
@@ -0,0 +1,7 @@
+package dao;
+import entities.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+@CrossOrigin
+public interface CountryRepository extends JpaRepository<Country, Long> {
+}
---
+++ src/main/java/dao/ExcursionRepository.java
@@ -0,0 +1,7 @@
+package dao;
+import entities.Excursion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+@CrossOrigin
+public interface ExcursionRepository extends JpaRepository<Excursion, Long> {
+}
